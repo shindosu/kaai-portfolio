badece391d938960a7e3f39a23a4e930a71cfac4
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2024-10-15 23:49:27 +0900

    delete things from plamic host

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,24 +1,5 @@
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '@/plasmic-init';
-import { HelloWorld } from "@/components/HelloWorld";
-import { WorkSlider } from "@/components/WorkSlider.jsx";
-
-PLASMIC.registerComponent(HelloWorld, {
-  name: "HelloWorld",
-  props: {
-    className: 'string',
-    verbose: 'boolean'
-  },
-  importPath: './components/HelloWorld',
-})
-
-PLASMIC.registerComponent(WorkSlider, {
-  name: "WorkSlider",
-  props: {
-    className: 'string',
-  },
-  importPath: './components/WorkSlider',
-});
 
 export default function PlasmicHost() {
   return PLASMIC && <PlasmicCanvasHost />;
